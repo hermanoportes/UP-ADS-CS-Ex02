59ca0471a335bd8b0f3e36a0859d88318f65910e
hermanoportes <hermanoportes@hotmail.com>
2018-07-14 19:57:27 -0300

    Renomeação intens anteriores

--- GestaoEscolar/Model/DAL/GestaoEscolarDB.Designer.cs
+++ GestaoEscolar/Model/DAL/GestaoEscolarDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Hermano\Source\Repos\UP-ADS-CS-Ex02\GestaoEscolar\Model\DAL\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Hermano\Source\Repos\UP-ADS-CS-Ex02\GestaoEscolar\Model\DAL\GestaoEscolarDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
