dc205d24ba586f73f7ed67dcff35482e5a772e05
hermanoportes <hermanoportes@hotmail.com>
2018-07-15 00:01:52 -0300

    Criação de controllers e métodos CRUD para Professores e Disciplinas.

---
+++ GestaoEscolar/AppGestaoEscolar/Controller/DisciplinasController.cs
@@ -0,0 +1,54 @@
+﻿using AppGestaoEscolar.Model.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestaoEscolar.Controller
+{
+    public class DisciplinasController
+    {
+        ModelGEscContainer contexto = new ModelGEscContainer();
+
+        public void Inserir(Disciplina disciplina)
+        {
+            contexto.Disciplinas.Add(disciplina);
+            contexto.SaveChanges();
+        }
+
+        public void Alterar(Disciplina disciplina)
+        {
+            contexto.Entry(disciplina).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void InserirAluno()
+        {
+
+        }
+
+        public void ExcluirAluno()
+        {
+
+        }
+
+        public void Excluir(int idDisciplina)
+
+        {
+            Disciplina disciplina = BuscarId(idDisciplina);
+            contexto.Disciplinas.Remove(disciplina);
+            contexto.SaveChanges();
+        }
+
+        public List<Disciplina> Listar()
+        {
+            return contexto.Disciplinas.ToList();
+        }
+
+        public Disciplina BuscarId(int idDisciplina)
+        {
+            return contexto.Disciplinas.Find(idDisciplina);
+        }   
+    }
+}
---
+++ GestaoEscolar/AppGestaoEscolar/Controller/ProfessoresController.cs
@@ -0,0 +1,44 @@
+﻿using AppGestaoEscolar.Model.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestaoEscolar.Controller
+{
+    public class ProfessoresController
+    {
+        ModelGEscContainer contexto = new ModelGEscContainer();
+
+        public void Inserir(Professor professor)
+        {
+            contexto.Professores.Add(professor);
+            contexto.SaveChanges();
+        }
+
+        public void Alterar(Professor professor)
+        {
+            contexto.Entry(professor).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Excluir(int idProfessor)
+
+        {
+            Professor professor = BuscarId(idProfessor);
+            contexto.Professores.Remove(professor);
+            contexto.SaveChanges();
+        }
+
+        public List<Professor> Listar()
+        {
+            return contexto.Professores.ToList();
+        }
+
+        public Professor BuscarId(int idProfessor)
+        {
+            return contexto.Professores.Find(idProfessor);
+        }
+    }
+}
